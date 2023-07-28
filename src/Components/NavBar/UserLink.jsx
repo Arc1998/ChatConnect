0385be1ac3d0ff82def741b7cba8e154101eeac6
Archana <archanagangurde.comp@gmail.com>
2023-07-28 23:05:09 +0530

    userlink updated

--- src/Components/NavBar/UserLink.jsx
+++ src/Components/NavBar/UserLink.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 
-//import { AuthContext } from "../AppContext/AppContext";
-
 const UserLink = () => {
- // const { signOutUser, user, userData } = useContext(AuthContext);
-
   return (
     <div className="flex justify-center items-center cursor-pointer">
       <div className="hover:translate-y-1 duration-500 ease-in-out hover:text-blue-500">
@@ -55,21 +51,6 @@ const UserLink = () => {
           />
         </svg>
       </div>
-      {/* <div className="mx-4 flex items-center" onClick={signOutUser}>
-        <Tooltip content="Sign Out" placement="bottom">
-          <Avatar
-            src={user?.photoURL || avatar}
-            size="sm"
-            alt="avatar"
-          ></Avatar>
-        </Tooltip>
-        <p className="ml-4 font-roboto text-sm text-black font-medium no-underline">
-          {user?.displayName === null && userData?.name !== undefined
-            ? userData?.name?.charAt(0)?.toUpperCase() +
-              userData?.name?.slice(1)
-            : user?.displayName?.split(" ")[0]}
-        </p>
-      </div> */}
     </div>
   );
 };
